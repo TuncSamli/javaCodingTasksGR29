d64f4382de551b1eea7ac70f4a78a9f5136fbdee
Tamerlan <thuseynov2017@gmail.com>
2022-02-26 22:35:55 -0500

    First week done!

--- src/Week1/finra.java
+++ src/Week1/finra.java
@@ -1,27 +1,17 @@
 package Week1;
 public class finra {
-    public static void main(String[] args) {
-        FINRA(1,30);// stating arguments
-    }
-    public static void FINRA(int num1, int num2) { //FINRA method and passed parameters
-        String rslt = "";// creating a string to collect result in the future
+    public static void finra(int a) {
+        for (int i = 1; i < a; i++) {
 
-        for (int i = num1; i <= num2; i++) {//loop to iterate from num1 to num2 including
-            if (i % 15 == 0) {// if a number equals to 15 a word "FINRA" will be printed
-                rslt += "FINRA ";// collecting FINRA
-
-            } else if (i % 5 == 0) {// if a number equals to 5 a word "FIN" will be printed
-
-                rslt +="FIN ";//collecting FIN
-            } else if (i%3  == 0){//if a number equals to 3 a word "RA" will be printed
-                rslt+="RA ";//collecting RA
-
-            }else{ //if no conditions above are not met it will return a number
-                rslt+=i+" ";
-
+            if (i % 15 == 0) {
+                System.out.println("FinRA");
+            } else if (i % 3 == 0) {
+                System.out.println("Fin");
+            } else if (i % 5 == 0) {
+                System.out.println("Ra");
+            } else {
+                System.out.println(i);
             }
         }
-        System.out.println(rslt);// printing result for user to observe
-
     }
 }
--- src/Week1/oddOrEven.java
+++ src/Week1/oddOrEven.java
@@ -1,11 +1,6 @@
 package Week1;
 public class oddOrEven {
-    public static void main(String[] args) {
-        System.out.println("number is " +oddOrEvenMethod(4));//print statement + argument is being set inside the statement
+    public  static String oddOrEvenMethod(int numberCheck){
+        return (numberCheck%2==0)?"Even":"Odd";
     }
-    public  static String oddOrEvenMethod(int numberCheck){//oddOrEven method and passed parameters
-        return (numberCheck%2==0)?"even":"odd";
-
-    }
-
 }
