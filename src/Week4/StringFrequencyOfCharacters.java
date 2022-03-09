11d727a477dad6da38a0f6209019fcfb56e4aa1c
Alex Akulov <[alexyiyi92@gmail.com]>
2022-03-09 17:49:32 -0500

    updated commit for string freq task

--- src/Week4/StringFrequencyOfCharacters.java
+++ src/Week4/StringFrequencyOfCharacters.java
@@ -8,18 +8,18 @@ public class StringFrequencyOfCharacters {
         Freq("AABBDDCDEE");
     }
     public static void Freq(String string){
-        Map<Character, Integer> newString = new HashMap<Character, Integer>();
-        for(int i = 0; i < string.length(); i++)
+        Map<Character, Integer> newString = new HashMap<Character, Integer>();// we will be storing all characters (keys) and their frequencies (values) inside this newly created map data structure
+        for(int i = 0; i < string.length(); i++)//iterating given string
         {
-            if(newString.containsKey(string.charAt(i)))
+            if(newString.containsKey(string.charAt(i)))//collecting chars from string inside our map data structure in a random order but maintaining size of the map based on the number of characters transferred
             {
-                newString.put(string.charAt(i), newString.get(string.charAt(i)) + 1);
+                newString.put(string.charAt(i), newString.get(string.charAt(i)) +1);//collecting chars/freqs+1 since they can appear more than 1 time
             }
             else
             {
-                newString.put(string.charAt(i), 1);
+                newString.put(string.charAt(i), 1);// otherwise, collecting characters that appeared only once
             }
-            System.out.println(newString);
+            System.out.println(newString);//tracking iteration for debugging and just fyi how it works
         }
         // Print characters and their
         // frequencies in same order
